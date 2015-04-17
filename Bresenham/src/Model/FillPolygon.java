55d69d33426bacbbd1dc9465254eb14094e026af
MChrista <mic007.ch@t-online.de>
2015-04-17 10:09:33 +0200

    Actual State
    TODO: Adding logic for filling polygons in class FillPolygon and call
    them from class MainWindow.

---
+++ Bresenham/src/Model/FillPolygon.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class FillPolygon {
+	public FillPolygon(){
+		
+	}
+	
+	public int [][] fill(int [][] matrix, int xStartpoint, int yStartpoint){
+		if(matrix.length<xStartpoint || matrix.length<yStartpoint){
+			System.out.println("Offset is not in range of array");
+			return null;
+		}else{
+			
+			
+			
+			
+			
+			
+			
+			return null;
+		}
+	}
+
+}
